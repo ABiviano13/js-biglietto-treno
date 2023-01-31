6f32554e7d6c224922796c0781f0dd9217ee47f8
ABiviano13 <abiviano0@outlook.com>
2023-01-31 19:25:41 +0100

    FEAT structure program js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// 1. Definire le variabili km - età
+// 2. Chiedere all'utente quanti km vuole percorrere
+// 3. Chiedere all'utente la propria età
+// 4. Sconto del 20% per i minorenni
+// 5. Sconto del 40% per gli over 65
+// 6. Calcolo per definire il prezzo (valore intero : max 2 decimali)
